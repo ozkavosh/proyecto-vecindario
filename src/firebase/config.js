24edc0ff7f77905bfd47f113d2a8a0bf125a88f3
ozkavosh <augustoslva@hotmail.com>
2022-09-20 11:38:20 -0300

    Firebase env edit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID
 };
 
