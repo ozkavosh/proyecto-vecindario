0597fd920632ad368176ad40dbd48664b7c421dd
ozkavosh <augustoslva@hotmail.com>
2022-09-22 20:56:51 -0300

    Propery short reviews added

--- src/components/Property/Property.jsx
+++ src/components/Property/Property.jsx
@@ -3,18 +3,22 @@ import "swiper/css";
 import "swiper/css/navigation";
 import {
   FaRegUserCircle,
-  FaRegCheckCircle ,
+  FaRegCheckCircle,
   FaRegHeart,
   FaRegPaperPlane,
   FaPenSquare,
   FaMapMarkerAlt,
-  FaChevronDown
+  FaChevronDown,
 } from "react-icons/fa";
 import { BiMessageEdit } from "react-icons/bi";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Review from "../Review/Review";
 import Stars from "../Stars/Stars";
+import { useState } from "react";
 
 const Property = ({ data }) => {
+  const [showReviews, setShowReviews] = useState(false);
+
   return (
     <div className="property">
       <div className="propertyHeader">
@@ -40,23 +44,34 @@ const Property = ({ data }) => {
           <FaRegPaperPlane />
         </div>
         <button type="button" className="addReviewBtn">
-          <FaPenSquare/> Nueva reseña
+          <FaPenSquare /> Nueva reseña
         </button>
       </div>
 
       <div className="propertyInfo">
         <h3 className="propertyType">{data.type.toUpperCase()}</h3>
         <div className="propertyLocation">
-          <FaMapMarkerAlt/>
+          <FaMapMarkerAlt />
           <p>{data.location}</p>
         </div>
-        <p className="propertyDescription">{data.description.length > 240 ? data.description.slice(0,240) + "..." : data.description}</p>
-        <div className="propertyReviewsButton">
-          <BiMessageEdit/>
+        <p className="propertyDescription">
+          {data.description.length > 240
+            ? data.description.slice(0, 240) + "..."
+            : data.description}
+        </p>
+        <div
+          className="propertyReviewsButton"
+          onClick={() => setShowReviews((prev) => !prev)}
+        >
+          <BiMessageEdit />
           Reseñas
-          <FaChevronDown/>
+          <FaChevronDown />
         </div>
       </div>
+
+      {showReviews && <div className="reviews">
+            {data.reviews.map(data => <Review key={data.id} data={data}/>)}
+        </div>}
     </div>
   );
 };
