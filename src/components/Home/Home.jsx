f71a3a13f7a8d47dfba1cf7d160c7ffea08c5884
gastoncarriquiry <tato.carri@gmail.com>
2022-10-09 21:36:26 -0300

    Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,12 +4,18 @@ import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { ReactComponent as KeyIcon } from "../../assets/svg/key.svg";
+import { ReactComponent as ChatIcon } from "../../assets/svg/chat_circle_dots.svg";
+import { ReactComponent as MGlassIcon } from "../../assets/svg/magnifying_glass.svg";
+import { ReactComponent as PencilIcon } from "../../assets/svg/pencil_line.svg";
 
 const Home = () => {
   return (
     <section className="home">
       <section className="welcome container">
-        <h1>¡Bienvenidos a Vecindario!</h1>
+        <h1>
+          ¡Bienvenidos a <strong>Vecindario</strong>!
+        </h1>
         <div className="search">
           <div className="searchbar">
             <FaSearch />
@@ -25,69 +31,103 @@ const Home = () => {
       <section className="find-your-place">
         <div className="container">
           <h2>Encontrá tu lugar ideal</h2>
-          <button>Registrarme / Iniciar sesión</button>
+          {/* <button>Registrarme / Iniciar sesión</button> */}
+          <p>
+            Conocé los mejores sitios donde alojarte a partir de publicaciones y de reseñas de otros
+            inquilinos
+          </p>
         </div>
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/proyectovecindario.appspot.com/o/carr1.png?alt=media&token=73f3495f-fcf5-4850-9f06-c8b6f3e83739"
+              alt=""
+            />
+          </SwiperSlide>
         </Swiper>
       </section>
       <section className="what-to-expect container">
-        <h2>¿Qué podrás encontrar en Vecindario?</h2>
+        <h2>
+          ¿Qué podrás encontrar en <strong>Vecindario</strong>?
+        </h2>
         <div>
           <article>
-            <div className="img"></div>
-            <img src="" alt="" />
-            <p>Amplio buscador por region, inmueble, reseñas o puntuación. Hecho a tu medida.</p>
-          </article>
-          <article>
-            <div className="img"></div>
-            <img src="" alt="" />
+            <div className="img">
+              <MGlassIcon />
+            </div>
             <p>
-              Muchas reseñas para conocer a tus vecinos, el barrio, condiciones del inmueble y mucho
-              más.
+              Amplio buscador por región, inmueble, reseñas, puntuación.{" "}
+              <strong>Hecho a tu medida</strong>
             </p>
           </article>
           <article>
-            <div className="img"></div>
-            <img src="" alt="" />
-            <p>100% seguro. Perfiles verificados, dueños e inquilinos reales para tu confianza.</p>
+            <div className="img">
+              <PencilIcon />
+            </div>
+            <p>
+              Muchas <strong>reseñas</strong> para conocer a tus vecinos, el barrio, condiciones del
+              inmueble y mucho más.
+            </p>
           </article>
           <article>
-            <div className="img"></div>
-            <img src="" alt="" />
+            <div className="img">
+              <KeyIcon />
+            </div>
             <p>
-              Te ayudamos con tus necesidades a través de un mapa interactivo que te brindará
-              soporte.
+              <strong>100% seguro</strong>. Perfiles verificados, dueños e inquilinos reales para tu
+              confianza.
+            </p>
+          </article>
+          <article>
+            <div className="img">
+              <ChatIcon />
+            </div>
+            <p>
+              Conectate con los propietarios u otros inquilinos a través del
+              <strong>chat</strong>.
             </p>
           </article>
         </div>
       </section>
-      <section className="most-searched container">
+      <section className="most-searched">
         <h2>Los más buscados</h2>
         <Swiper
           slidesPerView={3}
-          navigation={true}
-          spaceBetween={30}
+          navigation={false}
+          spaceBetween={5}
           modules={[Navigation]}
           className="mySwiper"
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/proyectovecindario.appspot.com/o/foto.scroll1.png?alt=media&token=8e986891-e311-40f7-ac25-6c63b30355a1"
+              alt="carr1"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/proyectovecindario.appspot.com/o/foto.scroll2.png?alt=media&token=36442c08-5087-4f36-b00b-2edd13712062"
+              alt="carr2"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/proyectovecindario.appspot.com/o/foto.scroll3.png?alt=media&token=21adba81-bf40-4b63-b773-59ae1b917543"
+              alt="carr3"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/proyectovecindario.appspot.com/o/foto.scroll4.png?alt=media&token=0719ef67-23c0-4dff-a381-7cf916db9da1"
+              alt="carr4"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/proyectovecindario.appspot.com/o/foto.scroll5.png?alt=media&token=2bae3abd-e111-4d74-a798-15724881e76c"
+              alt="carr5"
+            />
+          </SwiperSlide>
         </Swiper>
       </section>
       <section className="join-owners">
@@ -95,14 +135,18 @@ const Home = () => {
           <h2>¿Eres propietario?</h2>
           <button>¡Empieza ahora!</button>
           <p>
-            Te ayudamos a dar a conocer tu propiedad y obtener mayor llegada a potenciales
-            inquilinos.
+            Nosotros te ayudamos a darte a conocer publicando tu propiedad y obteniendo gran llegada
+            a potenciales inquilinos.
           </p>
         </div>
       </section>
       <section className="testimonies container">
         <div className="testimony">
-          <div>
+          <div className="testimony-profile">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/proyectovecindario.appspot.com/o/testimony1.png?alt=media&token=fc221719-75a8-4296-8efd-31c00be421e6"
+              alt=""
+            />
             <h3>Germán Pérez</h3>
             <p>33 años</p>
           </div>
@@ -116,7 +160,11 @@ const Home = () => {
             “Como propietaria intento siempre mejorar a partir de las reseñas que me dejan otros
             inquilinos.”
           </p>
-          <div>
+          <div className="testimony-profile">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/proyectovecindario.appspot.com/o/testimony2.png?alt=media&token=b06ff999-57de-4f42-8116-7c9fca0f96b2"
+              alt=""
+            />
             <h3>Jennifer Paz</h3>
             <p>52 años</p>
           </div>
